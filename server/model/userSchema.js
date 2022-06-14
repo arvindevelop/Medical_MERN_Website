f1a8cc2c909e01194749259126a22ae4c57a1b04
arvindevelop <aks7631353674@gmail.com>
2022-06-14 15:35:56 +0530

    User authenticatio

---
+++ server/model/userSchema.js
@@ -0,0 +1,77 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const validator = require('validator')
+dotenv.config({path:'./config.env'});
+
+const SECRET_KEY = process.env.SECRET_KEY;
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Please provide your workspace email.");
+            }
+        }
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    role:{
+        type: String,
+        required: true,
+        uppercase: true
+    },
+    creationDate:{
+        type : Date,
+        default: Date.now
+    },
+    lastLogged:{
+        type : Date,
+        default: Date.now
+    },
+    tokens:[
+        {
+            token:{
+                type: String,
+                required: true
+            }
+        }
+    ]
+})
+
+//Token generation
+userSchema.methods.generateAuthToken = async function(){
+
+    try {
+        const token = jwt.sign({_id:this._id.toString()},SECRET_KEY);
+        this.tokens = this.tokens.concat({token:token});
+        await this.save();
+        return token;
+    }
+    catch (error) {
+        //res.send("Error: " + error);
+        console.log("Error issue: " + error);
+    }
+}
+
+//hashing password
+userSchema.pre('save', async function(next){
+    if(this.isModified('password')){
+        this.password = bcrypt.hashSync(this.password, 12);
+    }
+    next();
+})
+
+const User = mongoose.model('User',userSchema);
+
+module.exports = User;
