a584b30a8d7dba2e755f54fc0496fad0184c9dc7
arvindevelop <aks7631353674@gmail.com>
2022-06-25 11:41:40 +0530

    changed

--- server/router/auth.js
+++ server/router/auth.js
@@ -35,7 +35,11 @@ router.post('/api/v1/auth/login',async (req,res) => {
             else{
                 //token generation
                 const token = await userLogin.generateAuthToken();
-                res.cookie("jwt",token,{ expires: new Date(Date.now() + (10 * 365 * 24 * 60 * 60))});
+                res.cookie("jwt",token,{ 
+                    expires: new Date(Date.now() + (10 * 365 * 24 * 60 * 60)),
+                    httpOnly:true,
+                    secure:true
+                });
 
                 await User.findByIdAndUpdate(userLogin._id,{'$set' : { 'lastLogged' : Date.now()} }, { new : true });
                 res.status(200).json({status:200, jwtToken:token});
